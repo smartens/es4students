a0bba93de0d332140ac4b10c612e5bf115b76b7c
Steffen Martens <steffen.martens@stud.uni-due.de>
2019-01-24 13:45:15 +0100

    Add Login bloc, event, state

---
+++ lib/login/login_bloc.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+
+import 'login_event.dart';
+import 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  @override
+  // TODO: implement initialState
+  get initialState => null;
+
+  @override
+  Stream mapEventToState(currentState, event) {
+    // TODO: implement mapEventToState
+    return null;
+  }
+
+}

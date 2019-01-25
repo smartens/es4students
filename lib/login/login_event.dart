174c68b758fb173243512a2ab2d30124c88a896d
Steffen Martens <steffen.martens@stud.uni-due.de>
2019-01-25 16:09:16 +0100

    Add comments to login events

--- lib/login/login_event.dart
+++ lib/login/login_event.dart
@@ -1,10 +1,13 @@
 import 'package:meta/meta.dart';
 import 'package:equatable/equatable.dart';
 
+/// Basisklasse für Events der Loginoberfläche
 abstract class LoginEvent extends Equatable {
   LoginEvent([List props = const []]) : super(props);
 }
 
+/// Eventklasse, welche an den Login Bloc weitergeleitet wird, 
+/// wenn der Login Button gedrückt wird.
 class LoginButtonPressed extends LoginEvent {
   final String username;
   final String password;

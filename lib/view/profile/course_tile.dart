8ff2c01e23c799391fa21dfc3b67871cf4b92727
skchleye <christian.leyens@stud.uni-due.de>
2019-02-04 18:04:40 +0100

    Add ProfilePage & CourseTile

---
+++ lib/view/profile/course_tile.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class CourseTile extends StatelessWidget {
+  CourseTile({Key key, @required this.name, @required this.description})
+      : super(key: key);
+
+  final String name;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.book),
+              title: Text(name),
+              subtitle: Text(description),
+            ),
+            ButtonTheme.bar(
+              child: ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: const Text('ABMELDEN'),
+                    onPressed: () {
+                      /* ... */
+                    },
+                  ),
+                  FlatButton(
+                    child: const Text('ZUM KURS'),
+                    onPressed: () {
+                      /* ... */
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/profile/profile_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+import 'course_tile.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    ///Methode um Kurse mit DummyData zu generieren
+    List<CourseTile> generateDummyData() {
+      List<CourseTile> dummyData = new List<CourseTile>();
+      for (int i = 0; i < 10; i++) {
+        dummyData.add(
+          CourseTile(
+            name: ('Kurs ' + i.toString()),
+            description: 'Lorem ipsum dolor sit amet, consetetur '
+                'sadipscing elitr, sed diam',
+          ),
+        );
+      }
+      return dummyData;
+    }
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            title: const Text('Übersicht'),
+            centerTitle: true,
+            expandedHeight: 256.0,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Stack(
+                fit: StackFit.loose,
+                children: <Widget>[
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 70.0,
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          /*Text(
+                            'Profil',
+                            style: Theme.of(context).textTheme.headline.apply(
+                              fontWeightDelta: 2,
+                            ),
+                          ),*/
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: CircleAvatar(
+                              child: Text(
+                                "VN",
+                                //TODO: Methode zum Generieren der Initialen
+                                style: TextStyle(fontSize: 48.0),
+                              ),
+                              backgroundColor: Colors.white,
+                              maxRadius: 56.0,
+                            ),
+                          ),
+                          Text(
+                            'Vorname Nachname', //TODO: Verweis für Name
+                            style:
+                                TextStyle(fontSize: 24.0, color: Colors.white),
+                          ),
+                          SizedBox(
+                            height: 8.0,
+                          ),
+                          Text(
+                            'Student', //TODO: Verweis für Status
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(generateDummyData()),
+          )
+        ],
+      ),
+    );
+  }
+}

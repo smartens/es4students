334a2d8e642e7da6b310eea0087fe73bd778ff28
skchleye <christian.leyens@stud.uni-due.de>
2019-04-07 16:15:27 +0200

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,78 @@
 import 'package:flutter/material.dart';
-import 'package:app/view/onboarding_page.dart';
 
+import 'package:app/view/onboarding_page.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'authentication/authentication_bloc.dart';
+import 'authentication/authentication_event.dart';
+import 'authentication/authentication_state.dart';
+import 'repository/user_repository.dart';
+import 'view/profile/profile_page.dart';
 import 'view/login/login_page.dart';
 
-void main() => runApp(MyApp());
+class SimpleBlocDelegate extends BlocDelegate {
+  @override
+  void onTransition(Transition transition) {
+    print(transition.toString());
+  }
+}
 
-class MyApp extends StatelessWidget {
+void main() {
+  BlocSupervisor().delegate = SimpleBlocDelegate();
+  runApp(MyApp(userRepository: UserRepository()));
+}
+
+class MyApp extends StatefulWidget {
+  final UserRepository userRepository;
+
+  MyApp({Key key, @required this.userRepository}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  AuthenticationBloc _authenticationBloc;
+
+  UserRepository get _userRepository => widget.userRepository;
+
+  @override
+  void initState() {
+    _authenticationBloc = AuthenticationBloc(userRepository: _userRepository);
+    _authenticationBloc.dispatch(AppStarted());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _authenticationBloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: OnBoardingPage(),
-      theme: ThemeData(
-        primaryColor: Color.fromRGBO(67, 84, 161, 1.0),
+    return BlocProvider<AuthenticationBloc>(
+      bloc: _authenticationBloc,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: BlocBuilder<AuthenticationEvent, AuthenticationState>(
+          bloc: _authenticationBloc,
+          builder: (BuildContext context, AuthenticationState state) {
+            if (state is AuthenticationUninitialized) {
+              return OnBoardingPage();
+            }
+            if (state is AuthenticationAuthenticated) {
+              return ProfilePage();
+            }
+            if (state is AuthenticationUnauthenticated) {
+              return LoginPage(userRepository: _userRepository,);
+            }
+          },
+        ),
+        theme: ThemeData(
+          primaryColor: Color.fromRGBO(67, 84, 161, 1.0),
+        ),
       ),
     );
   }

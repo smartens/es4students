0d13ce5b5c938d9a2fe532fdf255d522821a842f
Steffen Martens <steffen.martens@stud.uni-due.de>
2019-04-07 15:53:21 +0200

    login bloc geupdated und authentication bloc hinzugefuegt

--- lib/login/login_bloc.dart
+++ lib/login/login_bloc.dart
@@ -1,19 +1,42 @@
 import 'dart:async';
+import 'package:meta/meta.dart';
 
 import 'package:bloc/bloc.dart';
+import 'package:app/repository/user_repository.dart';
+import 'package:app/authentication/authentication_bloc.dart';
+import 'package:app/authentication/authentication_event.dart';
 
 import 'login_event.dart';
 import 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  @override
-  // TODO: implement initialState
-  get initialState => null;
+  final UserRepository userRepository;
+  final AuthenticationBloc authenticationBloc;
+
+  LoginBloc({
+    @required this.userRepository,
+    @required this.authenticationBloc,
+  })  : assert(userRepository != null),
+        assert(authenticationBloc != null);
+
+  LoginState get initialState => LoginInitial();
 
   @override
   Stream<LoginState> mapEventToState(LoginEvent event) async* {
-    // TODO: implement mapEventToState
-    return;
-  }
+    if (event is LoginButtonPressed) {
+      yield LoginLoading();
 
+      try {
+        /// Token besorgen von Moodle
+        final token = await userRepository.authenticate(
+            username: event.username, password: event.password);
+
+        authenticationBloc.dispatch(LoggedIn(token: token));
+        yield LoginInitial();
+
+      } catch (error) {
+        yield LoginFailure(error: error.toString());
+      }
+    }
+  }
 }

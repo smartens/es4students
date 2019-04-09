5aa1fe8335a3fab25bc1d91831996bc0ce74e733
SINGH <bhupindersingh@132.252.202.233>
2019-04-09 19:22:52 +0200

    feature: Moodle Authentication at LoginView

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -1,13 +1,31 @@
 import 'package:meta/meta.dart';
 import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class UserRepository {
+   String serviceName = "moodle_mobile_app";
+
   Future<String> authenticate({
     @required String username,
     @required String password,
   }) async {
-    await Future.delayed(Duration(seconds: 1));
-    return 'token';
+    
+    final String url = "https://moodle.uni-due.de/login/token.php?username=$username&password=$password&service=$serviceName";
+
+    var res = await http.get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
+    if(res != null)
+    {
+      Map jsonModel = jsonDecode(res.body);
+      if(jsonModel['token'] != null)
+      {
+        return jsonModel['token'];
+      }
+      else{
+       throw new Exception('Benutzername oder Passwort ist falsch!');
+      }
+    }
   }
 
   Future<void> deleteToken() async {

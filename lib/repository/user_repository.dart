ad532aa2aa18f535f708a8f51113c06cc6dd39c8
SINGH <bhupindersingh@132.252.202.233>
2019-04-09 19:53:03 +0200

    User_Repository Methods implemented.

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -2,15 +2,15 @@ import 'package:meta/meta.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserRepository {
    String serviceName = "moodle_mobile_app";
-
+   
   Future<String> authenticate({
     @required String username,
     @required String password,
   }) async {
-    
     final String url = "https://moodle.uni-due.de/login/token.php?username=$username&password=$password&service=$serviceName";
 
     var res = await http.get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
@@ -29,20 +29,28 @@ class UserRepository {
   }
 
   Future<void> deleteToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     /// delete from keystore/keychain
-    await Future.delayed(Duration(seconds: 1));
-    return;
+    prefs.remove('token');
   }
 
   Future<void> persistToken(String token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('token', token);
     /// write to keystore/keychain
     await Future.delayed(Duration(seconds: 1));
     return;
   }
 
   Future<bool> hasToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.getString('token') ?? false) {
+      return true;
+    } else {
+      return false;
+    }
     /// read from keystore/keychain
-    await Future.delayed(Duration(seconds: 1));
-    return false;
+    //await Future.delayed(Duration(seconds: 1));
+    //return false;
   }
 }

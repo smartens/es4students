a40f06458e8c6e95684ba04e20875b729389ce4b
skchleye <christian.leyens@stud.uni-due.de>
2019-04-23 18:47:42 +0200

    Name abrufen

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -4,27 +4,25 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-
 class UserRepository {
-   String serviceName = "moodle_mobile_app";
-   
+  String serviceName = "moodle_mobile_app";
+
   Future<String> authenticate({
     @required String username,
     @required String password,
   }) async {
-    final String url = "https://moodle.uni-due.de/login/token.php?username=$username&password=$password&service=$serviceName";
+    final String url =
+        "https://moodle.uni-due.de/login/token.php?username=$username&password=$password&service=$serviceName";
 
-    var res = await http.get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+    var res = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
 
-    if(res != null)
-    {
+    if (res != null) {
       Map jsonModel = jsonDecode(res.body);
-      if(jsonModel['token'] != null)
-      {
+      if (jsonModel['token'] != null) {
         return jsonModel['token'];
-      }
-      else{
-       throw new Exception('Benutzername oder Passwort ist falsch!');
+      } else {
+        throw new Exception('Benutzername oder Passwort ist falsch!');
       }
     }
     return "";
@@ -32,6 +30,7 @@ class UserRepository {
 
   Future<void> deleteToken() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
+
     /// delete from keystore/keychain
     prefs.remove('token');
   }
@@ -39,6 +38,7 @@ class UserRepository {
   Future<void> persistToken(String token) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString('token', token);
+
     /// write to keystore/keychain
     await Future.delayed(Duration(seconds: 1));
     return;
@@ -51,47 +51,49 @@ class UserRepository {
     } else {
       return false;
     }
+
     /// read from keystore/keychain
     //await Future.delayed(Duration(seconds: 1));
     //return false;
   }
 
-  Future<Map> getUserData() async { //liefert ein Map zurück. jsonModel['fullname'] ergibt die Name des Benutzers.
+  Future<String> getUserData(String key) async {
+    //liefert ein Map zurück. jsonModel['fullname'] ergibt die Name des Benutzers.
     SharedPreferences prefs = await SharedPreferences.getInstance();
     final String token = prefs.get('token');
 
     Map jsonModel;
-    final String url = "https://moodle.uni-due.de/webservice/rest/server.php?wstoken=$token.get&wsfunction=core_webservice_get_site_info&moodlewsrestformat=json";
+    final String url =
+        "https://moodle.uni-due.de/webservice/rest/server.php?wstoken=$token&wsfunction=core_webservice_get_site_info&moodlewsrestformat=json";
 
-    var res = await http.get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+    var res = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
 
-    if(res != null)
-    {
+    if (res != null) {
+      print("RES --> " + res.body);
       jsonModel = jsonDecode(res.body);
-    } else{
+    } else {
       throw new Exception('DashboardData Error!');
     }
-    return jsonModel;
+    return jsonModel[key];
   }
 
   Future<Map> getDashboardCourseData() async {
-   SharedPreferences prefs = await SharedPreferences.getInstance();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     final String token = prefs.get('token');
 
     Map jsonModel;
 
-    final String url = "https://es4students.de/elearning/webservice/rest/server.php?wstoken=$token&wsfunction=core_enrol_get_users_courses&moodlewsrestformat=json&userid=10";
-    var res = await http.get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+    final String url =
+        "https://es4students.de/elearning/webservice/rest/server.php?wstoken=$token&wsfunction=core_enrol_get_users_courses&moodlewsrestformat=json&userid=10";
+    var res = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
 
-    if(res != null)
-    
-    {
+    if (res != null) {
       jsonModel = jsonDecode(res.body);
-      
-    } else{
+    } else {
       throw new Exception('Dashboard CourseData Error!');
     }
     return jsonModel;
   }
-  }
-  
+}

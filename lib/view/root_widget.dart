ab489578996e7297a047080bbf6c3a545c8a3a65
skchleye <christian.leyens@stud.uni-due.de>
2019-04-28 12:01:08 +0200

    Update Root

--- lib/view/root_widget.dart
+++ lib/view/root_widget.dart
@@ -1,25 +1,44 @@
 import 'package:es4students/bloc/authentication/component.dart';
 import 'package:es4students/data/providers/moodle_client.dart';
 import 'package:es4students/data/repositories/user_repository.dart';
-import 'package:es4students/view/home.dart';
+import 'package:es4students/view/login/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
 
-class RootWidget extends StatelessWidget {
-  final UserRepository userRepository = UserRepository(
+class RootWidget extends StatefulWidget {
+  final UserRepository _userRepository = UserRepository(
     moodleApiClient: MoodleApiClient(
       httpClient: http.Client(),
     ),
   );
 
   @override
+  _RootWidgetState createState() => _RootWidgetState();
+}
+
+class _RootWidgetState extends State<RootWidget> {
+  AuthenticationBloc _authenticationBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _authenticationBloc = BlocProvider.of<AuthenticationBloc>(context);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return BlocProvider(
       bloc: AuthenticationBloc(),
       child: MaterialApp(
-        home: Home(),
+        home: LoginPage(userRepository: widget._userRepository,),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _authenticationBloc.dispose();
+    super.dispose();
   }
 }

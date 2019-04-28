3f4629df40927fa73ad8434440984a0c5b3f75d3
sgsnmart <steffen.martens@stud.uni-due.de>
2019-04-28 11:47:25 +0200

    add userrepository to rootwidget

--- lib/view/root_widget.dart
+++ lib/view/root_widget.dart
@@ -1,10 +1,18 @@
 import 'package:es4students/bloc/authentication/component.dart';
-import 'package:es4students/main.dart';
+import 'package:es4students/data/providers/moodle_client.dart';
+import 'package:es4students/data/repositories/user_repository.dart';
 import 'package:es4students/view/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 
 class RootWidget extends StatelessWidget {
+  final UserRepository userRepository = UserRepository(
+    moodleApiClient: MoodleApiClient(
+      httpClient: http.Client(),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(

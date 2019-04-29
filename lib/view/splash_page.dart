4c79658249eba764b9f1b1b4e39bbe95c254b271
sgsnmart <steffen.martens@stud.uni-due.de>
2019-04-29 13:27:45 +0200

    Wrap logo with Hero container on splash

--- lib/view/splash_page.dart
+++ lib/view/splash_page.dart
@@ -5,11 +5,13 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
+          child: Hero(
+        tag: 'logo',
         child: Image.asset(
           'images/erp4students_logo.png',
           fit: BoxFit.cover,
         ),
-      ),
+      )),
     );
   }
 }

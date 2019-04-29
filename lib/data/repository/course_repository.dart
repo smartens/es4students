3a9b3d2ed40129b512d9dbf62ab51d49f9f4016f
sgsnmart <steffen.martens@stud.uni-due.de>
2019-04-29 17:47:55 +0200

    Get token from sharedPrefs in CourseRepo/courselis

--- lib/data/repository/course_repository.dart
+++ lib/data/repository/course_repository.dart
@@ -6,11 +6,14 @@ import 'dart:async';
 
 class CourseRepository {
   final MoodleApiClient moodleApiClient;
+  SharedPreferences sharedPreferences;
 
   CourseRepository({@required this.moodleApiClient})
       : assert(moodleApiClient != null);
 
-  Future<List<Course>> getCourseList(String token) async {
+  Future<List<Course>> getCourseList() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    String token = sharedPreferences.getString('token');
     User user = await moodleApiClient.fetchUserData(token);
     return await moodleApiClient.fetchUserCourseList(token, user.userid);
   }
